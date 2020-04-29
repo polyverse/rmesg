f16bdccc0e76d3e8b8e92ca973e60649be454776
Archis Gore <archis@polyverse.com>
2020-04-29 00:59:23 -0700

    Compiles cross-platform, only works for Linux

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[cfg(target_os = "linux")]
 pub mod linux;
-
+#[cfg(target_os = "linux")]
 use std::convert::TryFrom;
+
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
@@ -47,55 +48,57 @@ impl std::convert::From<linux::KLogCtlError> for RMesgError {
 }
 
 // acts as a library when needed
+#[cfg(target_os = "linux")]
 fn rmesg() -> Result<String, RMesgError> {
-    if cfg!(target_os = "linux") {
-        let mut dummy_buffer: [i8; 0] = [];
-        let buf_size = linux::safe_klogctl(
-            linux::KLogType::SyslogActionSizeBuffer,
-            dummy_buffer.as_mut_ptr(),
-            dummy_buffer.len(),
-        )?;
-        let buf_usize = match usize::try_from(buf_size) {
-            Ok(u) => u,
-            Err(e) => {
-                return Err(RMesgError::IntegerOutOfBound(format!(
-                    "Error converting {} into a usize: {:?}",
-                    buf_size, e
-                )))
-            }
-        };
-
-        let mut real_buffer: Vec<u8> = vec![0; buf_usize];
-        let len_i32 = linux::safe_klogctl(
-            linux::KLogType::SyslogActionReadAll,
-            real_buffer.as_mut_ptr() as *mut i8,
-            real_buffer.capacity(),
-        )?;
-
-        if len_i32 < 0 {
-            return Err(RMesgError::KLogCtlInternalError(format!(
-                "Error from klogctl. Unable to read buffer. Code: {}",
-                len_i32
-            )));
+    let mut dummy_buffer: [i8; 0] = [];
+    let buf_size = linux::safe_klogctl(
+        linux::KLogType::SyslogActionSizeBuffer,
+        dummy_buffer.as_mut_ptr(),
+        dummy_buffer.len(),
+    )?;
+    let buf_usize = match usize::try_from(buf_size) {
+        Ok(u) => u,
+        Err(e) => {
+            return Err(RMesgError::IntegerOutOfBound(format!(
+                "Error converting {} into a usize: {:?}",
+                buf_size, e
+            )))
         }
-        let len_usize = match usize::try_from(len_i32) {
-            Ok(u) => u,
-            Err(e) => {
-                return Err(RMesgError::IntegerOutOfBound(format!(
-                    "Error converting {} into a usize: {:?}",
-                    len_i32, e
-                )))
-            }
-        };
+    };
 
-        real_buffer.resize(len_usize, 0);
+    let mut real_buffer: Vec<u8> = vec![0; buf_usize];
+    let len_i32 = linux::safe_klogctl(
+        linux::KLogType::SyslogActionReadAll,
+        real_buffer.as_mut_ptr() as *mut i8,
+        real_buffer.capacity(),
+    )?;
 
-        //adjust buffer capacity to what was read
-        real_buffer.resize(len_usize, 0);
-        Ok(String::from_utf8(real_buffer)?)
-    } else {
-        Err(RMesgError::NotImplementedForThisPlatform)
+    if len_i32 < 0 {
+        return Err(RMesgError::KLogCtlInternalError(format!(
+            "Error from klogctl. Unable to read buffer. Code: {}",
+            len_i32
+        )));
     }
+    let len_usize = match usize::try_from(len_i32) {
+        Ok(u) => u,
+        Err(e) => {
+            return Err(RMesgError::IntegerOutOfBound(format!(
+                "Error converting {} into a usize: {:?}",
+                len_i32, e
+            )))
+        }
+    };
+
+    real_buffer.resize(len_usize, 0);
+
+    //adjust buffer capacity to what was read
+    real_buffer.resize(len_usize, 0);
+    Ok(String::from_utf8(real_buffer)?)
+}
+
+#[cfg(not(target_os = "linux"))]
+fn rmesg() -> Result<String, RMesgError> {
+    Err(RMesgError::NotImplementedForThisPlatform)
 }
 
 fn main() {
