e2b2053b95c7ca3954dd87e63e1c52f4c03f6b13
Archis Gore <archis@polyverse.com>
2021-01-01 11:21:57 -0800

    Update build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#docker run --rm -it -v $PWD:/zerotect --privileged rust bash
 
-docker run -v cargo-cache:/root/.cargo/registry -v $PWD:/volume --rm -it --privileged polyverse/rust-dev-env
+IMAGE="ghcr.io/polyverse/rust-dev-env/rust-dev-env"
+docker run -v cargo-cache:/root/.cargo/registry -v $PWD:/volume --rm -it --privileged $IMAGE
