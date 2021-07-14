8850ab25f696e5b99b2b3d8327421bd8dc708ef1
Archis <archis@polyverse.com>
2021-07-14 01:19:54 -0700

    Update build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-IMAGE="ghcr.io/polyverse/rust-dev-env/rust-dev-env"
+IMAGE="ghcr.io/polyverse/rust-dev-env"
 docker run -v cargo-cache:/root/.cargo/registry -v $PWD:/volume --rm -it --privileged $IMAGE
